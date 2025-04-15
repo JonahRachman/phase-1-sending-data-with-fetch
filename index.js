e6c8299a7a43b192152dd304f43ef1b9b3f9faca
JonahRachman <jonahrachman76@gmail.com>
2025-04-15 09:03:44 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(name, email) {
+
+    const formData = {
+        name : name,
+        email : email,
+      };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+      };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      document.body.innerHTML += `<p> ${object.id} </p>`;
+    })
+    .catch(function (error) {
+      alert('Unauthorized Access');
+      document.body.innerHTML += `<p> ${error} </p>`;
+    });}
+  
+  
+  
